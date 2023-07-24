d6783f9590be9d2d195c63a130b072927c9d67aa
eric406-yeboah <ericusyeboah406@gmail.com>
2023-07-24 10:18:48 +0000

    assignment

---
+++ main2.py
@@ -0,0 +1,20 @@
+# def do_some_thing():
+#     print('hello')
+    
+# do_some_thing()
+
+# def addition(num1,num2):
+#     print(num1 +num2)
+    
+# addition(12,3)
+
+# def addition(num1,num2):
+#     return num1 + num2
+
+# sum=addition(12,3)
+
+def multiplication(num1,num2,num3):
+    return num1*num2*num3
+
+multiply=multiplication(2,3,5)
+print(multiply)
